949dcde1d71402c90b7b1c8a8eece5b568a7de90
Ranit Karmakar <rkarmaka@mtu.edu>
2024-10-15 20:59:45 -0400

    fix-analysis-metadata

--- biaqc/analysis.py
+++ biaqc/analysis.py
@@ -179,48 +179,75 @@ class MetadataAnalysis:
 
     def get_extension(self):
         n_extension, extensions = self._get_generic_info('extension')
+        
         if n_extension > 1:
             return f"[x] More than one image type found. Found extensions are {extensions}."
         return f"[v] All images are of {extensions[0]} type."
 
     def get_instrument(self):
         n_instrument, instruments = self._get_generic_info('instrument_model')
+
+        if n_instrument is None:
+            return f"[x] Could not find the instrument name."
+        
         if n_instrument > 1:
             return f"[x] More than one instrument found. Found instruments are {instruments}."
+        
         return f"[v] All images are acquired using {instruments[0]}."
 
     def get_lensNA(self):
         n_lensNA, lensNA = self._get_generic_info('objective_lens_na')
+
+        if n_lensNA is None:
+            return f"[x] Could not find the lens objective."
+        
         if n_lensNA > 1:
-            return f"[x] More than single lens NA found. Found lens NA are {lensNA}."
-        return f"[v] All images are acquired with {lensNA[0]}."
+            return f"[x] More than single lens objective found. Found lens objectives are {lensNA}."
+        
+        return f"[v] All images are acquired with {lensNA[0]} objective."
 
     def get_magnification(self):
         n_magnification, magnifications = self._get_generic_info('objective_nominal_magnification')
+
+        if n_magnification is None:
+            return f"[x] Could not find the lens magnification."
+        
         if n_magnification > 1:
             return f"[x] More than single magnification found. Found magnifications are {magnifications}."
         return f"[v] All images are acquired with {int(magnifications[0])}x."
 
     def get_bit_depth(self):
         n_bit_depth, bit_depths = self._get_generic_info('significant_bits')
+        if n_bit_depth is None:
+            return f"[x] Could not find bit depth."
+        
         if n_bit_depth > 1:
             return f"[x] More than single bit depth found. Found bit depths are {bit_depths}."
         return f"[v] All images are acquired with {bit_depths[0]}."
 
     def get_size_x(self):
         n_size_x, size_x = self._get_generic_info('size_x')
+        if n_size_x is None:
+            return f"[x] Could not find image width."
+        
         if n_size_x > 1:
-            return f"[x] Different image width found."
+            return f"[x] Different image widths found."
         return f"[v] Images have width {size_x[0]}."
 
     def get_size_y(self):
         n_size_y, size_y = self._get_generic_info('size_y')
+        if n_size_y is None:
+            return f"[x] Could not find image height."
+        
         if n_size_y > 1:
-            return f"[x] Different image height found."
+            return f"[x] Different image heights found."
         return f"[v] Images have height {size_y[0]}."
 
     def get_size_z(self):
         n_size_z, size_z = self._get_generic_info('size_z')
+        if n_size_z is None:
+            return f"[x] Could not find z-depth."
+        
         if n_size_z > 1:
             return f"[?] 3D z-stack with different z-depth."
         elif size_z != 1:
@@ -229,6 +256,8 @@ class MetadataAnalysis:
 
     def get_size_t(self):
         n_size_t, size_t = self._get_generic_info('size_t')
+        if n_size_t is None:
+            return f"[?] Could not find t or not a time series."
         if n_size_t > 1:
             return f"[?] Time series data with different time."
         elif size_t != 1:
@@ -237,6 +266,9 @@ class MetadataAnalysis:
 
     def get_size_c(self):
         n_size_c, size_c = self._get_generic_info('size_c')
+        if n_size_c is None:
+            return f"[x] Could not find number of channels."
+        
         if n_size_c > 1:
             return f"[x] Different number of channels found."
         elif size_c == 1:
@@ -245,22 +277,32 @@ class MetadataAnalysis:
 
     def get_physical_x(self):
         n_physical_size_x, physical_size_x = self._get_generic_info('physical_size_x')
+        if n_physical_size_x is None:
+            return f"[x] Could not find physical x size."
+        
         if n_physical_size_x > 1:
             return f"[x] Different physical size x found. Found physical size x are {np.round(physical_size_x, 4)}."
         return f"[v] All images acquired have {np.round(physical_size_x[0], 4)} micrometers physical size x."
 
     def get_physical_y(self):
         n_physical_size_y, physical_size_y = self._get_generic_info('physical_size_y')
+        if n_physical_size_y is None:
+            return f"[x] Could not find physical y size."
+        
         if n_physical_size_y > 1:
             return f"[x] Different physical size y found. Found physical size y are {np.round(physical_size_y, 4)}."
         return f"[v] All images acquired have {np.round(physical_size_y[0], 4)} micrometers physical size y."
 
     def get_delta_t(self):
-        diviser, unit = self._convert_time()
-        self.metadata['delta_time'] = np.round(self.metadata['delta_time'] / diviser, 2)
-        mean_time = self.metadata['delta_time'].mean()
-        std_time = self.metadata['delta_time'].std()
-        return f"[v] Time between frames: {np.round(mean_time, 4)} +/- {np.round(std_time, 4)} {unit}s."
+        try:
+            diviser, unit = self._convert_time()
+            self.metadata['delta_time'] = np.round(self.metadata['delta_time'] / diviser, 2)
+            mean_time = self.metadata['delta_time'].mean()
+            std_time = self.metadata['delta_time'].std()
+            return f"[v] Time between frames: {np.round(mean_time, 4)} +/- {np.round(std_time, 4)} {unit}s."
+        
+        except:
+            return f"[x] Cound not find time delta."
 
     def generate_report(self):
         """Generates a report by calling all functions and combining their outputs into a list of strings."""
